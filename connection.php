70dfe3c828fb3ef9e1914caec4a71aa0a86586ca
pikporamat <poramat.dc@gmail.com>
2020-05-16 07:07:42 +0700

    inittial commit

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$db_host="localhost";
+$db_user="sa";
+$db_password="sa";
+$db_name="pdo_crud_db";
+
+try{
+    $db = new PDO("mysql:host={$db_host}; dbname={$db_name}",$db_user, $db_password);
+    $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch(PDOEXCEPTION $e){
+    $e->getMessage();
+}
+?>
